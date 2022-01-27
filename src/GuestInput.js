f441b4ca68938f7eda1ac412e2b1e3a12babb0cf
katharina jäger <jaeger.kt@gmail.com>
2022-01-27 17:10:40 +0100

    Add functionality, display guestlist

--- src/GuestInput.js
+++ src/GuestInput.js
@@ -4,17 +4,47 @@ export default function GuestInput() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [guestList, setGuestList] = useState([]);
+  const [isChecked, setIsChecked] = useState(false);
 
-  const guest = firstName + lastName;
+  const guest = `${firstName} ${lastName}`;
 
   function handleAddGuest() {
     // needs to: - setGuestList to current first and last name values
+    setGuestList([...guestList, guest]);
     //           - setFirstName and setLastName to empty
+    setFirstName('');
+    setLastName('');
+  }
+
+  function handleRemoveGuest() {
+    const currentList = [...guestList];
+    currentList.pop();
+    setGuestList([...currentList]);
   }
 
   return (
     <div>
-      <div>Display the guestlist here</div>
+      <div>
+        Display the guestlist here
+        <div>
+          {guestList.map((singleGuest) => {
+            return (
+              <div key={singleGuest}>
+                <p>{singleGuest}</p>
+                <label>
+                  {' '}
+                  {isChecked ? 'Attending' : 'Not attending'}
+                  <input
+                    type="checkbox"
+                    checked={isChecked}
+                    onChange={() => setIsChecked(isChecked ? false : true)}
+                  />
+                </label>
+              </div>
+            );
+          })}
+        </div>
+      </div>
 
       <div data-test-id="guest">
         <label>
@@ -35,7 +65,7 @@ export default function GuestInput() {
       </div>
 
       <button onClick={handleAddGuest}>Add guest</button>
-      <button>Remove guest</button>
+      <button onClick={handleRemoveGuest}>Remove last guest</button>
     </div>
   );
 }

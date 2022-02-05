dab46a977bb028d8574cf1cc9c43ff5c9d489e45
katharina jäger <jaeger.kt@gmail.com>
2022-02-05 18:50:45 +0100

    Add image credit and style it

--- src/Guestlist.js
+++ src/Guestlist.js
@@ -323,6 +323,16 @@ export default function Guestlist() {
           </button>
         </div>
       </div>
+      <p className="credit">
+        Background image by{' '}
+        <a href="https://unsplash.com/@anniespratt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Annie Spratt
+        </a>{' '}
+        on{' '}
+        <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Unsplash
+        </a>
+      </p>
     </div>
   );
 }
